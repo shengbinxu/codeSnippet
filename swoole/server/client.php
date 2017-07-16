f8374717dd46a3dfed7c421af24850fae453504a
shengbinxu <shengbin_xu@163.com>
2017-07-16 10:59:54 +0800

    kill进程组

---
+++ swoole/server/client.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xushengbin
+ * Date: 2017/7/16
+ * Time: 10:15
+ */
+$fp = fsockopen("tcp://127.0.0.1", 9501, $errno, $errstr, 30);
+if (!$fp) {
+    echo "$errstr ($errno)<br />\n";
+} else {
+    echo 'write';
+    fwrite($fp, "hello\n");
+    echo fread($fp,100);
+    fclose($fp);
+}
---
+++ swoole/server/server.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * swoole server
+ * Created by PhpStorm.
+ * User: xushengbin
+ * Date: 2017/7/16
+ * Time: 9:48
+ */
+
+$serv = new swoole_server('127.0.0.1', 9501, SWOOLE_PROCESS,SWOOLE_SOCK_TCP);
+$serv->set(array(
+    'worker_num' => 4,
+    'daemonize' => true,
+    'backlog' => 128,
+));
+$serv->on('Connect', 'my_onConnect');
+$serv->on('Receive', 'my_onReceive');
+$serv->on('Close', 'my_onClose');
+function my_onConnect(){
+    trigger_error('on connect' . "\r\n");
+}
+function my_onReceive(){
+    trigger_error('on receive' . "\r\n");
+}
+function my_onClose(){
+    trigger_error('on close' . "\r\n");
+}
+$serv->start();
