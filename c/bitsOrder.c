aac3f5c8391847cc1d9ae1d4b578efee3fa1d0c5
许胜斌 <xushengbin@chelun.com>
2017-07-17 11:40:38 +0800

    c

--- c/bitsOrder.c
+++ c/bitsOrder.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
+#include<iostream>  
+#include <WinSock2.h>#include <arpa/inet.h>
+using namespace std;
 int main() {
-	printf("hello World!");
+	int a = 0x04030201;
+	printf("%i\n",a);
+	int n_a = htonl(a);
+	char *p = (char *)&a;
+	//char * s = "hello World!";
+	printf("%i\n", p[0]);
+	printf("%x\n", p[1]);
+	printf("%x\n", p[2]);
+	printf("%i\n", p[3]);
+	getchar();
+	return 0;
 }
