a60beb564c3eda7c480b70a9d27a39a4acdfff38
shengbinxu <shengbin_xu@163.com>
2017-08-18 15:00:20 +0800

    php inheritance this

---
+++ php_demo/inheritance.php
@@ -0,0 +1,38 @@
+<?php
+class Child extends  Parents{
+    public function coreComponents()
+    {
+        return array_merge(parent::coreComponents(), [
+            'request' => ['class' => 'yii\web\Request'],
+            'response' => ['class' => 'yii\web\Response'],
+            'session' => ['class' => 'yii\web\Session'],
+            'user' => ['class' => 'yii\web\User'],
+            'errorHandler' => ['class' => 'yii\web\ErrorHandler'],
+        ]);
+    }
+}
+
+class  Parents{
+    function __construct()
+    {
+        print_r($this);
+        print_r($this->coreComponents());
+    }
+    public function coreComponents()
+    {
+        return [
+            'log' => ['class' => 'yii\log\Dispatcher'],
+            'view' => ['class' => 'yii\web\View'],
+            'formatter' => ['class' => 'yii\i18n\Formatter'],
+            'i18n' => ['class' => 'yii\i18n\I18N'],
+            'mailer' => ['class' => 'yii\swiftmailer\Mailer'],
+            'urlManager' => ['class' => 'yii\web\UrlManager'],
+            'assetManager' => ['class' => 'yii\web\AssetManager'],
+            'security' => ['class' => 'yii\base\Security'],
+        ];
+    }
+}
+
+$child = new Child(); //execute child coreComponents()
+
+//$parent = new Parents(); //execute parent coreComponents()
